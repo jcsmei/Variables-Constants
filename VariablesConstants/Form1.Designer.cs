625f3428bd68ef46bbd44f0256bdb3b8c1af5fd7
jcsme <jcsme@DESKTOP-O6725QL>
2017-02-12 13:36:13 -0500

    Add project files.

---
+++ VariablesConstants/Form1.Designer.cs
@@ -0,0 +1,341 @@
+﻿namespace VariablesConstants
+{
+    partial class frmVariablesContants
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblInput = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lblOut = new System.Windows.Forms.Label();
+            this.lblWords = new System.Windows.Forms.Label();
+            this.lblWholeNumbers = new System.Windows.Forms.Label();
+            this.lblAnyNumbers = new System.Windows.Forms.Label();
+            this.btnCharacter = new System.Windows.Forms.Button();
+            this.btnString = new System.Windows.Forms.Button();
+            this.btnByte = new System.Windows.Forms.Button();
+            this.btnShort = new System.Windows.Forms.Button();
+            this.btnInt = new System.Windows.Forms.Button();
+            this.btnLong = new System.Windows.Forms.Button();
+            this.btnBool = new System.Windows.Forms.Button();
+            this.lblBoolean = new System.Windows.Forms.Label();
+            this.btnFloat = new System.Windows.Forms.Button();
+            this.btnDouble = new System.Windows.Forms.Button();
+            this.btnDecimal = new System.Windows.Forms.Button();
+            this.btnDvide3 = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInput.Location = new System.Drawing.Point(29, 13);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(80, 19);
+            this.lblInput.TabIndex = 0;
+            this.lblInput.Text = "User Input";
+            this.lblInput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtInput
+            // 
+            this.txtInput.BackColor = System.Drawing.Color.White;
+            this.txtInput.Location = new System.Drawing.Point(24, 35);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(474, 20);
+            this.txtInput.TabIndex = 1;
+            // 
+            // lblOut
+            // 
+            this.lblOut.AutoSize = true;
+            this.lblOut.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOut.Location = new System.Drawing.Point(32, 85);
+            this.lblOut.Name = "lblOut";
+            this.lblOut.Size = new System.Drawing.Size(59, 19);
+            this.lblOut.TabIndex = 3;
+            this.lblOut.Text = "Output";
+            this.lblOut.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblWords
+            // 
+            this.lblWords.AutoSize = true;
+            this.lblWords.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWords.Location = new System.Drawing.Point(24, 285);
+            this.lblWords.Name = "lblWords";
+            this.lblWords.Size = new System.Drawing.Size(133, 19);
+            this.lblWords.TabIndex = 4;
+            this.lblWords.Text = "Letters and Words";
+            this.lblWords.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblWholeNumbers
+            // 
+            this.lblWholeNumbers.AutoSize = true;
+            this.lblWholeNumbers.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWholeNumbers.Location = new System.Drawing.Point(174, 285);
+            this.lblWholeNumbers.Name = "lblWholeNumbers";
+            this.lblWholeNumbers.Size = new System.Drawing.Size(120, 19);
+            this.lblWholeNumbers.TabIndex = 5;
+            this.lblWholeNumbers.Text = "Whole Numbers";
+            this.lblWholeNumbers.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblAnyNumbers
+            // 
+            this.lblAnyNumbers.AutoSize = true;
+            this.lblAnyNumbers.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnyNumbers.Location = new System.Drawing.Point(312, 285);
+            this.lblAnyNumbers.Name = "lblAnyNumbers";
+            this.lblAnyNumbers.Size = new System.Drawing.Size(102, 19);
+            this.lblAnyNumbers.TabIndex = 6;
+            this.lblAnyNumbers.Text = "Any Numbers";
+            this.lblAnyNumbers.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnCharacter
+            // 
+            this.btnCharacter.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCharacter.Location = new System.Drawing.Point(27, 312);
+            this.btnCharacter.Name = "btnCharacter";
+            this.btnCharacter.Size = new System.Drawing.Size(110, 39);
+            this.btnCharacter.TabIndex = 7;
+            this.btnCharacter.Text = "&char";
+            this.btnCharacter.UseVisualStyleBackColor = true;
+            this.btnCharacter.Click += new System.EventHandler(this.btnCharacter_Click);
+            // 
+            // btnString
+            // 
+            this.btnString.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnString.Location = new System.Drawing.Point(27, 363);
+            this.btnString.Name = "btnString";
+            this.btnString.Size = new System.Drawing.Size(110, 39);
+            this.btnString.TabIndex = 8;
+            this.btnString.Text = "&string";
+            this.btnString.UseVisualStyleBackColor = true;
+            this.btnString.Click += new System.EventHandler(this.btnString_Click);
+            // 
+            // btnByte
+            // 
+            this.btnByte.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnByte.Location = new System.Drawing.Point(178, 312);
+            this.btnByte.Name = "btnByte";
+            this.btnByte.Size = new System.Drawing.Size(106, 39);
+            this.btnByte.TabIndex = 9;
+            this.btnByte.Text = "&byte";
+            this.btnByte.UseVisualStyleBackColor = true;
+            this.btnByte.Click += new System.EventHandler(this.btnByte_Click);
+            // 
+            // btnShort
+            // 
+            this.btnShort.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnShort.Location = new System.Drawing.Point(178, 363);
+            this.btnShort.Name = "btnShort";
+            this.btnShort.Size = new System.Drawing.Size(106, 39);
+            this.btnShort.TabIndex = 10;
+            this.btnShort.Text = "&short";
+            this.btnShort.UseVisualStyleBackColor = true;
+            this.btnShort.Click += new System.EventHandler(this.btnShort_Click);
+            // 
+            // btnInt
+            // 
+            this.btnInt.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInt.Location = new System.Drawing.Point(178, 412);
+            this.btnInt.Name = "btnInt";
+            this.btnInt.Size = new System.Drawing.Size(106, 39);
+            this.btnInt.TabIndex = 11;
+            this.btnInt.Text = "&int";
+            this.btnInt.UseVisualStyleBackColor = true;
+            this.btnInt.Click += new System.EventHandler(this.btnInt_Click);
+            // 
+            // btnLong
+            // 
+            this.btnLong.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLong.Location = new System.Drawing.Point(178, 467);
+            this.btnLong.Name = "btnLong";
+            this.btnLong.Size = new System.Drawing.Size(106, 39);
+            this.btnLong.TabIndex = 12;
+            this.btnLong.Text = "&long";
+            this.btnLong.UseVisualStyleBackColor = true;
+            this.btnLong.Click += new System.EventHandler(this.btnLong_Click);
+            // 
+            // btnBool
+            // 
+            this.btnBool.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBool.Location = new System.Drawing.Point(24, 467);
+            this.btnBool.Name = "btnBool";
+            this.btnBool.Size = new System.Drawing.Size(110, 39);
+            this.btnBool.TabIndex = 13;
+            this.btnBool.Text = "&bool";
+            this.btnBool.UseVisualStyleBackColor = true;
+            this.btnBool.Click += new System.EventHandler(this.btnBool_Click);
+            // 
+            // lblBoolean
+            // 
+            this.lblBoolean.AutoSize = true;
+            this.lblBoolean.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBoolean.Location = new System.Drawing.Point(25, 432);
+            this.lblBoolean.Name = "lblBoolean";
+            this.lblBoolean.Size = new System.Drawing.Size(65, 19);
+            this.lblBoolean.TabIndex = 14;
+            this.lblBoolean.Text = "Boolean";
+            this.lblBoolean.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnFloat
+            // 
+            this.btnFloat.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFloat.Location = new System.Drawing.Point(316, 312);
+            this.btnFloat.Name = "btnFloat";
+            this.btnFloat.Size = new System.Drawing.Size(98, 39);
+            this.btnFloat.TabIndex = 15;
+            this.btnFloat.Text = "&float";
+            this.btnFloat.UseVisualStyleBackColor = true;
+            this.btnFloat.Click += new System.EventHandler(this.btnFloat_Click);
+            // 
+            // btnDouble
+            // 
+            this.btnDouble.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDouble.Location = new System.Drawing.Point(316, 363);
+            this.btnDouble.Name = "btnDouble";
+            this.btnDouble.Size = new System.Drawing.Size(98, 39);
+            this.btnDouble.TabIndex = 16;
+            this.btnDouble.Text = "d&ouble";
+            this.btnDouble.UseVisualStyleBackColor = true;
+            this.btnDouble.Click += new System.EventHandler(this.btnDouble_Click);
+            // 
+            // btnDecimal
+            // 
+            this.btnDecimal.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDecimal.Location = new System.Drawing.Point(316, 412);
+            this.btnDecimal.Name = "btnDecimal";
+            this.btnDecimal.Size = new System.Drawing.Size(98, 39);
+            this.btnDecimal.TabIndex = 17;
+            this.btnDecimal.Text = "&decimal";
+            this.btnDecimal.UseVisualStyleBackColor = true;
+            this.btnDecimal.Click += new System.EventHandler(this.btnDecimal_Click);
+            // 
+            // btnDvide3
+            // 
+            this.btnDvide3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDvide3.Location = new System.Drawing.Point(316, 468);
+            this.btnDvide3.Name = "btnDvide3";
+            this.btnDvide3.Size = new System.Drawing.Size(98, 39);
+            this.btnDvide3.TabIndex = 18;
+            this.btnDvide3.Text = "Di&vide By 3";
+            this.btnDvide3.UseVisualStyleBackColor = true;
+            this.btnDvide3.Click += new System.EventHandler(this.btnDvide3_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.Yellow;
+            this.btnClear.Font = new System.Drawing.Font("Arial Narrow", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(440, 312);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(59, 53);
+            this.btnClear.TabIndex = 19;
+            this.btnClear.Text = "CLEA&R";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Red;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(425, 462);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(74, 44);
+            this.btnExit.TabIndex = 20;
+            this.btnExit.Text = "E&XIT";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.BackColor = System.Drawing.Color.Black;
+            this.lblOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOutput.ForeColor = System.Drawing.Color.Lime;
+            this.lblOutput.Location = new System.Drawing.Point(22, 103);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(477, 157);
+            this.lblOutput.TabIndex = 21;
+            // 
+            // frmVariablesContants
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 523);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnDvide3);
+            this.Controls.Add(this.btnDecimal);
+            this.Controls.Add(this.btnDouble);
+            this.Controls.Add(this.btnFloat);
+            this.Controls.Add(this.lblBoolean);
+            this.Controls.Add(this.btnBool);
+            this.Controls.Add(this.btnLong);
+            this.Controls.Add(this.btnInt);
+            this.Controls.Add(this.btnShort);
+            this.Controls.Add(this.btnByte);
+            this.Controls.Add(this.btnString);
+            this.Controls.Add(this.btnCharacter);
+            this.Controls.Add(this.lblAnyNumbers);
+            this.Controls.Add(this.lblWholeNumbers);
+            this.Controls.Add(this.lblWords);
+            this.Controls.Add(this.lblOut);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblInput);
+            this.Name = "frmVariablesContants";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInput;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label lblOut;
+        private System.Windows.Forms.Label lblWords;
+        private System.Windows.Forms.Label lblWholeNumbers;
+        private System.Windows.Forms.Label lblAnyNumbers;
+        private System.Windows.Forms.Button btnCharacter;
+        private System.Windows.Forms.Button btnString;
+        private System.Windows.Forms.Button btnByte;
+        private System.Windows.Forms.Button btnShort;
+        private System.Windows.Forms.Button btnInt;
+        private System.Windows.Forms.Button btnLong;
+        private System.Windows.Forms.Button btnBool;
+        private System.Windows.Forms.Label lblBoolean;
+        private System.Windows.Forms.Button btnFloat;
+        private System.Windows.Forms.Button btnDouble;
+        private System.Windows.Forms.Button btnDecimal;
+        private System.Windows.Forms.Button btnDvide3;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblOutput;
+    }
+}
+
---
+++ VariablesConstants/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VariablesConstants
+{
+    public partial class frmVariablesContants : Form
+    {
+        public frmVariablesContants()
+        {
+            InitializeComponent();
+        }
+        const string descrbingOutput =
+            @"User input has been saved in a vaiable of data type";
+        const string describingDivideBy3Output =
+            @"User input has been saved in variables of three different datatypes: float, oduble and decimal.
+When the three variables are dicided by 3 and the resulting quotients are displayed below.";
+private void btnCharacter_Click(object sender, EventArgs e)
+        {
+            lblOutput.Text = "";
+            string userInput = txtInput.Text;
+            char Input =' ';
+            try
+            {
+                Input = char.Parse(userInput);
+                lblOutput.Text = descrbingOutput +
+                    " char. The variable is displayed below:" +
+                    Environment.NewLine + Input.ToString();
+            }
+            catch (Exception ex)
+            {
+                lblOutput.Text = ex.Message;
+            } 
+        }
+
+        private void btnString_Click(object sender, EventArgs e)
+        {
+            lblOutput.Text = "";
+            string userInput = txtInput.Text;
+            lblOutput.Text = descrbingOutput + " string. he variable is deisplay below:" +
+                Environment.NewLine + userInput;
+        }
+
+        private void btnBool_Click(object sender, EventArgs e)
+        {
+            lblOutput.Text = "";
+            string userInput = txtInput.Text;
+            bool Input = true;
+            try
+            {
+                Input = bool.Parse(userInput);
+                lblOutput.Text = descrbingOutput + " bool. The variable is display below:" +
+                    Environment.NewLine + Input.ToString();
+            }
+            catch (Exception ex)
+            {
+                lblOutput.Text = ex.Message;
+            }
+        }
+
+        private void btnByte_Click(object sender, EventArgs e)
+        {
+            lblOutput.Text = "";
+            string userInput = txtInput.Text;
+            byte Input = 0;
+            try
+            {
+                Input = byte.Parse(userInput);
+                lblOutput.Text = descrbingOutput +
+                    " byte. The variable is displayed below:" +
+                    Environment.NewLine + Input.ToString();
+            }
+            catch (Exception ex)
+            {
+                lblOutput.Text = ex.Message;
+            }
+        }
+
+        private void btnShort_Click(object sender, EventArgs e)
+        {
+            lblOutput.Text = "";
+            string userInput = txtInput.Text;
+            short Input = 0;
+            try
+            {
+                Input = short.Parse(userInput);
+                lblOutput.Text = descrbingOutput +
+                    " short. The variable is displayed below:" +
+                    Environment.NewLine + Input.ToString();
+            }
+            catch (Exception ex)
+            {
+                lblOutput.Text = ex.Message;
+            }
+
+        }
+
+        private void btnInt_Click(object sender, EventArgs e)
+        {
+            lblOutput.Text = "";
+            string userInput = txtInput.Text;
+            int Input = 0;
+            try
+            {
+                Input = int.Parse(userInput);
+                lblOutput.Text = descrbingOutput +
+                    " int. The variable is displayed below:" +
+                    Environment.NewLine + Input.ToString();
+            }
+            catch (Exception ex)
+            {
+                lblOutput.Text = ex.Message;
+            }
+        }
+
+        private void btnLong_Click(object sender, EventArgs e)
+        {
+
+            lblOutput.Text = "";
+            string userInput = txtInput.Text;
+            long Input = 0;
+            try
+            {
+                Input = long.Parse(userInput);
+                lblOutput.Text = descrbingOutput +
+                    " long. The variable is displayed below:" +
+                    Environment.NewLine + Input.ToString();
+            }
+            catch (Exception ex)
+            {
+                lblOutput.Text = ex.Message;
+
+            }
+        }
+
+        private void btnFloat_Click(object sender, EventArgs e)
+        {
+            
+                lblOutput.Text = "";
+                string userInput = txtInput.Text;
+                float Input = 0;
+                try
+                {
+                    Input = float.Parse(userInput);
+                    lblOutput.Text = descrbingOutput +
+                        " float. The variable is displayed below:" +
+                        Environment.NewLine + Input.ToString();
+                }
+                catch (Exception ex)
+                {
+                    lblOutput.Text = ex.Message;
+                }
+            }
+
+        private void btnDouble_Click(object sender, EventArgs e)
+        {
+            {
+                lblOutput.Text = "";
+                string userInput = txtInput.Text;
+                double Input = ' ';
+                try
+                {
+                    Input = double.Parse(userInput);
+                    lblOutput.Text = descrbingOutput +
+                        " double. The variable is displayed below:" +
+                        Environment.NewLine + Input.ToString();
+                }
+                catch (Exception ex)
+                {
+                    lblOutput.Text = ex.Message;
+                }
+            }
+        }
+
+        private void btnDecimal_Click(object sender, EventArgs e)
+        {
+            {
+                lblOutput.Text = "";
+                string userInput = txtInput.Text;
+                decimal Input = 0;
+                try
+                {
+                    Input = decimal.Parse(userInput);
+                    lblOutput.Text = descrbingOutput +
+                        " decimal. The variable is displayed below:" +
+                        Environment.NewLine + Input.ToString();
+                }
+                catch (Exception ex)
+                {
+                    lblOutput.Text = ex.Message;
+                }
+            }
+        }
+
+        private void btnDvide3_Click(object sender, EventArgs e)
+        {
+            lblOutput.Text = "";
+            string userInput = txtInput.Text;
+            float fInput = 0;
+            double dblInput = 0;
+            decimal decInput = 0;
+            try
+            {
+                float.TryParse(userInput, out fInput);
+                double.TryParse(userInput, out dblInput);
+                decimal.TryParse(userInput, out decInput);
+                lblOutput.Text = describingDivideBy3Output +
+                    Environment.NewLine + userInput + " / 3 = " +
+                    (fInput / 3).ToString() + " (using float variable;)" +
+                    Environment.NewLine + userInput + " / 3 = " +
+                    (dblInput / 3).ToString() + " (using double variable;)" +
+                    Environment.NewLine + userInput + " / 3 = " +
+                    (decInput / 3).ToString() + " (using decimal variables.)";
+
+            }
+            catch (Exception ex)
+            {
+                lblOutput.Text = ex.Message;
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lblOutput.Text = "";
+            txtInput.Clear();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
